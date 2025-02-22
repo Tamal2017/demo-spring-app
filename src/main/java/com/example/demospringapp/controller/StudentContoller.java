92a63aed12d0e1292b525a2fc714974f45e787d7
Joseph TAMO <tingangt@gmail.com>
2025-02-22 12:10:35 -0500

    Update UserController: change @PathParam to @PathVariable

--- src/main/java/com/example/demospringapp/controller/StudentContoller.java
+++ src/main/java/com/example/demospringapp/controller/StudentContoller.java
@@ -1,9 +1,9 @@
 package com.example.demospringapp.controller;
 
 import com.example.demospringapp.model.Student;
-import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +30,7 @@ public class StudentContoller {
     }
 
     @DeleteMapping("/student/{id}")
-    boolean deleteStudentById(@PathParam("id") int id) {
+    boolean deleteStudentById(@PathVariable("id") int id) {
         return students.removeIf(student -> student.getId() == id);
     }
 }
