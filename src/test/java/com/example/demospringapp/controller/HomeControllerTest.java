de8e8077792584dcb7800113af0252247acec2aa
Joseph TAMO <tingangt@gmail.com>
2025-07-26 12:18:11 -0400

    Add HomeController unit test

--- src/test/java/com/example/demospringapp/controller/HomeControllerTest.java
+++ src/test/java/com/example/demospringapp/controller/HomeControllerTest.java
@@ -1,12 +1,9 @@
 package com.example.demospringapp.controller;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -15,11 +12,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-@Disabled
+//@Disabled
 class HomeControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
