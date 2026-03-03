570d0067baf1b554a5bcba6a8f00c0c92fb0c22d
Joseph TAMO <tingangt@gmail.com>
2026-03-02 22:37:06 -0500

    :construction: Fixing tests

--- src/test/java/com/example/demospringapp/controller/HomeControllerTest.java
+++ src/test/java/com/example/demospringapp/controller/HomeControllerTest.java
@@ -3,7 +3,9 @@ package com.example.demospringapp.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -12,9 +14,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc(addFilters = false)
-//@Disabled
+@ActiveProfiles("test")
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -22,7 +24,6 @@ class HomeControllerTest {
     @Test
     void testGreet() throws Exception {
         this.mockMvc.perform(get("/"))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello welcome!")));
     }
